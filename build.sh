2208241bee5514f91988192c2a3d20ed0afb1fff
MyselfLeo <myselfleo@outlook.fr>
2022-01-10 18:15:11 +0100

    edited build.sh file

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Version info
+VERSION=1.0.0
+
+# Clear files
 rm -r build dist \~
+
+# Build
 pyinstaller src/gtit.py
+
+# Create final folder and move files
+mkdir package
+tar -zcvf "package/gtit-$VERSION.tar" dist/gtit
